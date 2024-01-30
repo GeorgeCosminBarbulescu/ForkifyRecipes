599f74577387816e490d85b31586e53e7eeb0e71
George Cosmin Bărbulescu <129797706+GeorgeCosminBarbulescu@users.noreply.github.com>
2024-01-30 11:12:57 +0200

    Update controller.js module
    Update "controlAddRecipe" function in controller.js module, corrected success message for newly created recipe, as it was displayed twice after recipe creation. Now is displayed only once.

--- src/js/controller.js
+++ src/js/controller.js
@@ -114,9 +114,11 @@ const controlAddRecipe = async function (newRecipe) {
     window.history.pushState(null, '', `#${model.state.recipe.id}`);
 
     // Close form window
+    /*
     setTimeout(function () {
       addRecipeView.toggleWindow();
     }, MODAL_CLOSE_SEC * 1000);
+    */
   } catch (err) {
     console.error('💥', err);
     addRecipeView.renderErrorMessage(err.message);
